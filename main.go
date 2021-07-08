fe999327a228197f5fd81e0bd2a116e8f6104bc1
takumi_kurogi <takumines1@gmail.com>
2021-07-08 15:25:39 +0900

    CORSの設定

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"time"
+
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/takumines/gin-jwt-auth/db"
 	"github.com/takumines/gin-jwt-auth/routes"
@@ -9,6 +12,36 @@ import (
 func main() {
 	r := gin.Default()
 	db.Init()
+
+	// CORS設定
+	r.Use(cors.New(cors.Config{
+		// 許可したいHTTPメソッドの一覧
+		AllowMethods: []string{
+			"POST",
+			"GET",
+			"PUT",
+			"DELETE",
+			"OPTIONS",
+		},
+		// 許可したいHTTPリクエストヘッダの一覧
+		AllowHeaders: []string{
+			"Access-Control-Allow-Headers",
+			"Content-Type",
+			"Content-Length",
+			"Accept-Encoding",
+			"X-CSRF-Token",
+			"Authorization",
+		},
+		// 許可したいアクセス元の一覧
+		AllowOrigins: []string{
+			"http://localhost:8080",
+		},
+		// cookieなどの情報を必要とするかどうか
+		AllowCredentials: true,
+		// preflightリクエストの結果をキャッシュする時間
+		MaxAge: 24 * time.Hour,
+	}))
+
 	routes.Setup(r)
 	r.Run(":8080")
 }
