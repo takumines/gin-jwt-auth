a106eaec1a7dc29eaeb9686574b68f223764020e
takumi_kurogi <takumines1@gmail.com>
2021-07-08 11:40:44 +0900

    gin install

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello Worls")
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello World",
+		})
+	})
+	r.Run(":8080")
 }
